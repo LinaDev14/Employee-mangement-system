d7e1d0c50b63e24e7a295a43a1dfd95789c6feb0
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-08 18:23:59 -0500

    se crea conexion a base de datos

---
+++ src/main/java/com/linadev14/employee/EmployeeSystemAppApplication.java
@@ -0,0 +1,13 @@
+package com.linadev14.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSystemAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSystemAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linadev14/employee/EmployeeSystemAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linadev14.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSystemAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
