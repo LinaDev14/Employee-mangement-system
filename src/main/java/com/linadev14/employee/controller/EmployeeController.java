989db5613c9e3dc14db95c065d53ee84e712e9bb
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-09 14:39:55 -0500

    se agrega conexion a proyecto react

--- src/main/java/com/linadev14/employee/controller/EmployeeController.java
+++ src/main/java/com/linadev14/employee/controller/EmployeeController.java
@@ -3,13 +3,12 @@ package com.linadev14.employee.controller;
 import com.linadev14.employee.model.Employee;
 import com.linadev14.employee.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/")
+// ruta de proyecto de react
+@CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
 public class EmployeeController {
 
