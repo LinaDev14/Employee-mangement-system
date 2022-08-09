e07611b1f6429a5fcc78cb4a35b70fcb5714355f
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-09 11:32:39 -0500

    se crea employee

--- src/main/java/com/linadev14/employee/model/Employee.java
+++ src/main/java/com/linadev14/employee/model/Employee.java
@@ -1,3 +1,14 @@
 package com.linadev14.employee.model;
+import lombok.Data;
+
+@Data
 public class Employee {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String numberPhone;
+    private String country;
+
 }
--- src/main/java/com/linadev14/employee/repository/EmployeeRepository.java
+++ src/main/java/com/linadev14/employee/repository/EmployeeRepository.java
@@ -1,8 +1,9 @@
 package com.linadev14.employee.repository;
 
+import com.linadev14.employee.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository {
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
 }
