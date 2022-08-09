789291b79619923cbd67ffef0091c4f3c39d58e3
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-09 12:00:57 -0500

    se crea metodo createEmployee

--- src/main/java/com/linadev14/employee/controller/EmployeeController.java
+++ src/main/java/com/linadev14/employee/controller/EmployeeController.java
@@ -1,7 +1,10 @@
 package com.linadev14.employee.controller;
 
+import com.linadev14.employee.model.Employee;
 import com.linadev14.employee.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
 
     private final EmployeeService employeeService;
+
+    @PostMapping("employees")
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeService.createEmployee(employee);
+    }
 }
--- src/main/java/com/linadev14/employee/service/EmployeeServiceInterface.java
+++ src/main/java/com/linadev14/employee/service/EmployeeServiceInterface.java
@@ -1,3 +1,7 @@
 package com.linadev14.employee.service;
+import com.linadev14.employee.model.Employee;
+
 public interface EmployeeServiceInterface {
+    Employee createEmployee(Employee employee);
+
 }
